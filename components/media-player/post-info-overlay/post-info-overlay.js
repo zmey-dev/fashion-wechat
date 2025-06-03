f798e05e67e4bd2a8a58a01f200693eab50d6faa
Zmey <zmey.dev1126@gmail.com>
2025-06-03 07:35:49 -0700

    fix:complete media player style

--- components/media-player/post-info-overlay/post-info-overlay.js
+++ components/media-player/post-info-overlay/post-info-overlay.js
@@ -1,6 +1,7 @@
 // components/post-info-overlay/post-info-overlay.js
 Component({
   properties: {
+    currentPost: { type: Object, value: {} },
     displayTitle: { type: String, value: "" },
     displayContent: { type: String, value: "" }
   }
